5cb84b3c6b1117e5338a84c1924e34264c95e350
r3moon <100349700+r3moon@users.noreply.github.com>
2022-02-27 18:22:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-p=2
-a=56
-y=89
-k=124
-S=(p*a)/(y**2*k)
-print('S=', S)
+a=3
+print(a)
+
+list1=[10,20,30,77]
+print(list1)
+
+london={'name': 'London1', 'location': 'London Str'}
+print(london)
+
+tuple=('1234',)
+print(tuple)
+
+r = [5999, 20, 80, 50, 568, 5999]
+set(r)
+set1 = set(r)
+print(set1)
+
+stroka="ochen vashniy text"
+print(stroka)
+
